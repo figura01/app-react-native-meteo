821c86c6367ef2d1c7e7abe6598a9f3632107225
laurent vuillaume <figura.graphik@gmail.com>
2025-05-01 15:19:20 +0200

    add fctns to get user coordinate and featch weather from api

--- pages/Home/Home.jsx
+++ pages/Home/Home.jsx
@@ -1,20 +1,61 @@
+import { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
 import { s } from './Home.style.js';
+import { requestForegroundPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import { MeteoAPI } from '../../api/meteo.js';
 
 function Home() {
-  return (
-    <>
+    const [coords, setCoords] = useState();
+    const [weather, setWeather] = useState();
 
-        <View style={s.meteo_basic}>
-            <Text style={s.meteo_basic.txt}>Meteo Basic</Text>
-        </View>
-        <View style={s.searchbar_container}>
-            <Text>Searchbar</Text>
-        </View>
-        <View style={s.meteo_advance}>
-            <Text>Meteo Advance</Text>
-        </View>
-    </>
-  );
+    useEffect(() => {
+        getUserCoords();
+    }, []);
+
+    useEffect(() => {
+        if(coords) {
+            fetchWeather(coords);
+        }
+    }, [coords])
+
+    async function getUserCoords() {
+        let { status } = await requestForegroundPermissionsAsync();
+        if (status === 'granted') {
+
+            const location = await getCurrentPositionAsync({});
+            setCoords({
+                lat: location.coords.latitude,
+                lng: location.coords.longitude
+            });
+            setCoords({lat: "48.85", lng: "2.35"});
+            return;
+        } else {
+            setCoords({lat: "48.85", lng: "2.35"});
+        }
+    }
+
+    async function fetchWeather(coordinates) {
+        const weatherResponse = await MeteoAPI.fetchWeatherFromCoords(coordinates);
+    
+        if (weatherResponse) {
+            setWeather(weatherResponse);
+        }
+    }
+    console.log("weather value: ", weather);
+
+    return (
+        <>
+
+            <View style={s.meteo_basic}>
+                <Text style={s.meteo_basic.txt}>Meteo Basic</Text>
+            </View>
+            <View style={s.searchbar_container}>
+                <Text>Searchbar</Text>
+            </View>
+            <View style={s.meteo_advance}>
+                <Text>Meteo Advance</Text>
+            </View>
+        </>
+    );
 }
 export default Home;
