9cd1f7b5c70f0565ccf0671d5d004d0bd1a7e37d
laurent vuillaume <figura.graphik@gmail.com>
2025-05-03 15:36:09 +0200

    upgrade App.js and App.style.js

--- App.js
+++ App.js
@@ -5,13 +5,20 @@ import Home from './pages/Home/Home.jsx';
 import { s } from './App.style.js';
 import { ImageBackground } from 'react-native';
 import backgroundImg from './assets/background.png';
+
+import AlataRegular from './assets/fonts/Alata-Regular.ttf';
+import { useFonts } from 'expo-font';
+
 export default function App() {
+  const [isFontsLoaded] = useFonts({
+    "Alata-Regular": AlataRegular,
+  });
   
   return (
     <ImageBackground source={backgroundImg} style={s.image_background} imageStyle={s.img} resizeMode="cover">
       <SafeAreaProvider>
         <SafeAreaView style={s.container}>
-          <Home />
+          {isFontsLoaded ? <Home /> : null}
         </SafeAreaView>
       </SafeAreaProvider>
     </ImageBackground>
